c47ec67db7da0a08d6592c35ce7f5cef8d2d44cb
dianatamayo <dmtamayo23@misena.edu.co>
2017-03-02 18:23:35 -0500

    header

--- views/include/header.php
+++ views/include/header.php
@@ -6,5 +6,3 @@
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
     </head>
-
-    <body>
