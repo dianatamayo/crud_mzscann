b1c59d5503f0febaab0ccacfaa7873424c10e5ef
dianatamayo <dmtamayo23@misena.edu.co>
2017-03-02 18:26:46 -0500

    footer

---
+++ views/include/footer.php
@@ -0,0 +1,9 @@
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
+   <script>
+       $(document).ready(function() {
+           $('select').material_select();
+       });
+   </script>
+</body>
+</html>
