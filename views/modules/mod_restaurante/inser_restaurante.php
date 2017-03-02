b893213eb2a2534aa65b27e4fc5ec5267e61799b
dianatamayo <dmtamayo23@misena.edu.co>
2017-03-02 18:03:22 -0500

    insertar_restaurante

---
+++ views/modules/mod_restaurante/inser_restaurante.php
@@ -0,0 +1,15 @@
+<form method="post" action="?c=restaurante&a=create" >
+	<div class="row">
+        <div class="input-field col s6 offset-s3">
+		  <h1 class="center ">Insertar Restaurante</h1>
+			<input type="text" placeholder="Nombre" name="data[]">
+			<input type="text" placeholder="Telefono" name="data[]">
+			<input type="text" placeholder="Direccion" name="data[]">
+			<input type="text" placeholder="Horario" name="data[]">
+			<input type="text" placeholder="Cantidad de mesas" name="data[]">
+		    <div class="center">
+			<button class="waves-effect waves-light btn">Guardar</button>
+			</div>
+		</div>
+	</div>
+</form>
