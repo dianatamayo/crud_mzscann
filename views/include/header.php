06a7b20adbb2deb68429dd109eeffe083c6de9dd
dianatamayo <dmtamayo23@misena.edu.co>
2017-03-02 18:20:26 -0500

    header

---
+++ views/include/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
+    </head>
+
+    <body>
