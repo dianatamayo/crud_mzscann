3711cf86b33df3fd61373df479b2f5bdb7b09de6
dianatamayo <dmtamayo23@misena.edu.co>
2017-03-02 18:14:51 -0500

    main_restaurante

---
+++ views/modules/mod_restaurante/main_restaurante.php
@@ -0,0 +1,4 @@
+<div class="center" style="margin-top: 25em">
+	<a href="?c=restaurante&a=viewCreate" class="waves-effect waves-light btn">CREAR</a>
+	<a href="?c=restaurante&a=read" class="waves-effect waves-light btn">LEER</a>
+</div>	
